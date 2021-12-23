84a9c91d40169a20df7c40b0d77c92fad670d01b
saar120 <saar120@gmail.com>
2021-12-23 18:29:47 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import CheckoutPage from "./Pages/CheckoutPage";
-import Homepage from "./Pages/Homepage";
+import Navbar from "./Components/Navbar/Navbar";
+import Categories from "./Pages/CategoriesPage/Categories";
+import Category from "./Pages/CategoryPage/Category";
+import CheckoutPage from "./Pages/CheckoutPage/CheckoutPage";
+import Homepage from "./Pages/HomePage/Homepage";
 
 export default class App extends Component {
   render() {
     return (
       <Router>
+        <Navbar />
         <Switch>
           <Route path="/" exact>
             <Homepage />
           </Route>
           <Route path="/checkout" exact>
             <CheckoutPage />
+          </Route>
+          <Route path="/categories" exact>
+            <Categories />
+          </Route>
+          <Route path="/categories/:category" exact>
+            <Category />
           </Route>
         </Switch>
       </Router>
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "../Navbar/Navbar.css";
+
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className="Navbar">
+        <div className="left-side">
+          <Link to="/" className="Link">
+            <i className="fas fa-home fa-2x"></i>
+          </Link>
+          <Link to="/categories" className="Link">
+            CATEGORIES
+          </Link>
+        </div>
+        <Link to="/checkout" className="Link">
+          <i className="fas fa-shopping-cart fa-2x"></i>
+        </Link>
+      </div>
+    );
+  }
+}
