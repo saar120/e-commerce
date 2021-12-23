d4e24c66bc300515a4f7bfeebcba505efc04696a
saar120 <saar120@gmail.com>
2021-12-23 17:36:11 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import CheckoutPage from "./Pages/CheckoutPage";
+import Homepage from "./Pages/Homepage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Homepage />
+          </Route>
+          <Route path="/checkout" exact>
+            <CheckoutPage />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
-
-export default App;
---
+++ src/Pages/CheckoutPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CheckoutPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Checkout Page</h1>
+      </div>
+    );
+  }
+}
